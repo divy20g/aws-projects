ae1cff9baca85b8e07d11b206bad3acdd5e3e038
divy20g <divy20g@gmail.com>
2023-02-27 13:19:54 -0600

    scripts3

--- s3_reader_class.py
+++ s3_reader_class.py
@@ -39,3 +39,33 @@ class S3ParquetReader:
     
     s3_reader = S3ParquetReader(bucket_name='my-bucket', aws_access_key_id='my-access-key', aws_secret_access_key='my-secret-key')
 df = s3_reader.read_parquet('path/to/my/file.parquet')
+
+#-----------------------
+import pyarrow.parquet as pq
+import pandas as pd
+import boto3
+
+class S3ParquetReader:
+    def __init__(self, s3_bucket, s3_prefix):
+        self.s3_bucket = s3_bucket
+        self.s3_prefix = s3_prefix
+        self.s3_client = boto3.client('s3')
+
+    def get_object_keys(self):
+        response = self.s3_client.list_objects_v2(Bucket=self.s3_bucket, Prefix=self.s3_prefix)
+        return [obj['Key'] for obj in response['Contents']]
+
+    def read_parquet_files(self):
+        s3_keys = self.get_object_keys()
+        df_list = []
+        for key in s3_keys:
+            obj = self.s3_client.get_object(Bucket=self.s3_bucket, Key=key)
+            table = pq.read_table(obj['Body'])
+            df = table.to_pandas()
+            df_list.append(df)
+        return pd.concat(df_list, ignore_index=True)
+    
+  #-------
+reader = S3ParquetReader('my-s3-bucket', 'my-s3-prefix')
+df = reader.read_parquet_files()  
+
